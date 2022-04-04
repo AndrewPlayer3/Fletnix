f5c2583387087e292e95f35eadb6341be236291c
Andrew Player <andrewplayer3@gmail.com>
2022-04-04 14:23:18 -0800

    remove new secrets

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    mongodburl: "mongodb://localhost:27017",
-    NEXTAUTH_URL: "http://localhost:3000",
-    JWT_SECRET: 'VerySecretString'
+
   },
   images: {
     domains: ["i.imgur.com", "i.ytimg.com"],
