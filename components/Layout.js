934e1fbaeb8b67e5a5c8bac57ea618a29cf701eb
Andrew Player <andrewplayer3@gmail.com>
2022-04-04 15:27:24 -0800

    remove router on dashboard and profile

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,10 @@
 import NavBar from "./common/Navbar/Navbar"
 import Footer from "../components/common/Footer"
 import Header from "../components/common/Header"
+import { useRouter } from 'next/router'
 
 export default function Layout({children, pageProps}){
+    const router = useRouter();
     return(
         <div className='flex max-w-screen'> 
             <div className="fixed top-0 w-screen drop-shadow-lg z-40">
@@ -11,9 +13,14 @@ export default function Layout({children, pageProps}){
             <div className='mb-16 mt-14'>
                 <main>{children}</main>
             </div>
-            <div className="fixed bottom-0">
-                <Footer />
-            </div>
+            { router.pathname.substring(0, 10) !== '/dashboard' && router.pathname.substring(0, 8) !== '/profile' ? 
+                <div className="fixed bottom-0">
+                    <Footer />
+                </div>
+                :
+                (console.log(router.pathname.substring(0, 10)))
+                (console.log(router.pathname.substring(0, 10)))
+            }
         </div>
     )
 }
