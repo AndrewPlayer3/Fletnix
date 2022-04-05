f90e5928c89e5b9107e5f7643b6a726b3468001c
Andrew Player <andrewplayer3@gmail.com>
2022-04-05 04:03:55 -0800

    fixed rating system

--- pages/api/rate.js
+++ pages/api/rate.js
@@ -3,6 +3,16 @@ import User from '../../models/user';
 import Video from '../../models/video';
 import { getSession } from 'next-auth/react'
 
+const get_and_set_previous_rating = (ratings, video_id) => {
+    let previous_rating = 0;
+    ratings.ratings.forEach(( rating ) => {
+        if (rating.video_id == video_id) {
+            previous_rating = rating.rating;
+        }
+    });
+    return Number(previous_rating);
+}
+
 const handler = async (req, res) => {
     if (req.method === 'POST') {   // Set Video Information | TODO: Only CONTENT_EDITORs should be able to make these requests.
 
@@ -11,13 +21,18 @@ const handler = async (req, res) => {
         const { rating, id } = JSON.parse(req.body);
 
         try {
-            const ratings = await User.findOne({email: user.email, ratings: {"$elemMatch": {video_id: id}}});
+            const ratings = await User.findOne({email: user.email, ratings: {"$elemMatch": {video_id: id}}}, 'ratings');
+            const rating_num = Number(rating);
             if (!ratings) {
                 const user_info = await User.findOneAndUpdate({email: user.email}, {"$push": {ratings: {video_id: id, rating: rating}}});
+                const rate_video = await Video.findByIdAndUpdate(id, { $inc: { 'analytics.total_rating': rating_num, 'analytics.total_ratings': 1 } });
                 return res.status(200).send({"rated": "true"});
             } else {
-                const like_video = await Video.findByIdAndUpdate(id, { $inc: { 'analytics.total_rating': - rating } });
-                const inc_likes  = await Video.findByIdAndUpdate(id, { $inc: { 'analytics.num_ratings' : - 1 } });
+                const previous_rating = get_and_set_previous_rating(ratings, id);
+                if (previous_rating !== rating_num) {
+                    const user_rating = await User.findOneAndUpdate({email: user.email, ratings: {"$elemMatch": {video_id: id } } }, {"$set": { 'ratings.$.rating': rating_num } } );
+                    const rate_video = await Video.findByIdAndUpdate(id, { $inc: { 'analytics.total_rating': rating_num - previous_rating } });
+                }
                 return res.status(200).send({"rated": "true"});
             }
         } catch (error) {
