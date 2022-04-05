554eed962c1e0474758857678ff12b88119e0bba
Andrew Player <andrewplayer3@gmail.com>
2022-04-05 02:19:16 -0800

    role guard replaced

--- pages/profile.js
+++ pages/profile.js
@@ -1,39 +1,22 @@
-import { getSession } from 'next-auth/react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 import Profile from '../components/Profile'
 import Layout from '../components/Layout'
-
-export async function getServerSideProps(context) {
-
-    const session = await getSession(context);
-
-    const res = await fetch('https://fletnix.vercel.app/api/user', {
-        method: 'GET',
-        headers: {
-            "Content-Type": "application/json",
-            cookie: context.req.headers.cookie,
-        },
-    });
-
-    const data = await res.json();
-
-    const user = {
-        username: data.username,
-        email: data.email,
-        role: data.role
-    }
-
-    return {
-        props: {
-            user: user
-        },
-    }
-}
+import loginStatus from '../helpers/login-status'
 
 export default function ProfilePage( { user } ) {
+    const { data: session, status } = useSession();
+    const router = useRouter();
     return (
-        <div className='absolute w-2/4 h-2/4 left-1/4 top-1/4'>
-            <Profile user = { user } />
-        </div>
+        <>
+        { loginStatus(status, router) ?
+            <div className='absolute w-2/4 h-2/4 left-1/4 top-1/4'>
+                <Profile user = { session.user } />
+            </div>
+            :
+            <></>
+        }
+        </>
     )
 }
 
