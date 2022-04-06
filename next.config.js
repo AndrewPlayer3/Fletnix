d3aa6c911faf502156ed039a55dd4ec0f311ed4d
Andrew Player <andrewplayer3@gmail.com>
2022-04-06 14:14:38 -0800

    updated env vars

--- next.config.js
+++ next.config.js
@@ -2,9 +2,16 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-
+    mongodburl: process.env.mongodburl,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+    GOOGLE_STORAGE: process.env.GOOGLE_STORAGE,
+    GOOGLE_PROJECT_ID: process.env.GOOGLE_PROJECT_ID,
+    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY,
+    GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
+    GOOGLE_BUCKET_NAME: process.env.GOOGLE_BUCKET_NAME
   },
   images: {
-    domains: ["i.imgur.com", "i.ytimg.com"],
+    domains: ["i.imgur.com", "i.ytimg.com", "storage.googleapis.com"],
   }
 }
