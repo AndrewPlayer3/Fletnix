fd08c167d586125bab92cddd114f7d68465e4772
Andrew Player <andrewplayer3@gmail.com>
2022-04-08 16:11:13 -0800

    removed unused portion of profile

--- components/Profile.js
+++ components/Profile.js
@@ -35,19 +35,6 @@ export default function profile({ user }) {
                                 <dt className="text-sm font-medium text-gray-500">Role</dt>
                                 <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{roles}</dd>
                             </div>
-                            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt className="text-sm font-medium text-gray-500">Password</dt>
-                                <dd className="flex justify-between text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                    ************
-                                    <PencilIcon className="flex-shrink-0 ml-16 h-5 w-5 text-gray-600" aria-hidden="true" />
-                                </dd>
-                            </div>
-                            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt className="text-sm font-medium text-gray-500">Your Stats</dt>
-                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                    Maybe we will store user watchtime, history, liked/disliked and so on.
-                                </dd>
-                            </div>
                         </dl>
                     </div>
                 </div>
