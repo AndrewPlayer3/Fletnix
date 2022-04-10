b8b141c53db668f2ad0c5326ed19a9ce73674083
Andrew Player <andrewplayer3@gmail.com>
2022-04-10 03:15:22 -0800

    removed sidebar added dash to menu

--- components/Profile.js
+++ components/Profile.js
@@ -16,12 +16,12 @@ export default function profile({ user }) {
 
         return (
             <div className='flex mt-8 items-center justify-center'>
-                <div className="bg-white shadow pr-12 overflow-hidden sm:rounded-lg">
-                    <div className="px-4 py-5 sm:px-6">
+                <div className="bg-white shadow pr-12 overflow-hidden rounded-lg">
+                    <div className="whitespace-nowrap px-4 py-5 sm:px-6">
                         <h3 className="text-2xl leading-6 font-medium text-gray-900">Your profile</h3>
                         <p className="mt-1 max-w-2xl text-md text-gray-500">Personal details.</p>
                     </div>
-                    <div className="border-t border-gray-200">
+                    <div className="border-t whitespace-nowrap border-gray-200">
                         <dl>
                             <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                 <dt className="text-md font-medium text-gray-500">Username</dt>
