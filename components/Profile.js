609c526c2a819c74a2ac168272e20ff09db78c28
Andrew Player <andrewplayer3@gmail.com>
2022-04-10 03:47:41 -0800

    fixed conflict

--- components/Profile.js
+++ components/Profile.js
@@ -34,9 +34,6 @@ export default function profile({ user }) {
                             <div className="bg-slate-300 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                 <dt className="text-md font-medium text-gray-500">Role</dt>
                                 <dd className="mt-1 text-md text-gray-900 sm:mt-0 sm:col-span-2">{roles}</dd>
-                            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                <dt className="text-sm font-medium text-gray-500">Role</dt>
-                                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{roles}</dd>
                             </div>
                         </dl>
                     </div>
