a80a8e75adcb00da7add9737623b11cdff91741d
Andrew Player <andrewplayer3@gmail.com>
2022-04-18 03:13:17 -0800

    added more get functions for redis

--- pages/api/video_redis.js
+++ pages/api/video_redis.js
@@ -1,8 +1,23 @@
-import { createVideo } from '../../lib/redis'
+import { createVideo, getAllVideos, getVideoById, getVideoBySearch } from '../../lib/redis'
+import { connect } from '../../lib/redis'
 
 export default async function handler(req, res) {
+    
+    await connect();
 
-        const {title, description, tags, filename, thumbnail, created_at, total_watchtime, total_rating, num_ratings, views} = req.body;
+    if (req.method == 'POST') {
+        const {
+            title,
+            description,
+            tags,
+            filename,
+            thumbnail,
+            created_at,
+            total_watchtime,
+            total_rating,
+            num_ratings,
+            views,
+        } = req.body
 
         const video = {
             title: title,
@@ -14,9 +29,27 @@ export default async function handler(req, res) {
             total_watchtime: total_watchtime,
             total_rating: total_rating,
             num_ratings: num_ratings,
-            views: views
+            views: views,
         }
-    
-        const id = await createVideo(video);
+
+        const id = await createVideo(video)
         return res.status(200).json({ id })
+    } else if (req.method == 'GET') {
+        const { id, search } = req.query
+        let videos;
+        if (id) {
+            videos = await getVideoById(id);
+        } else if (search) {
+            videos = await getVideoBySearch(search);
+        } else {
+            videos = await getAllVideos();
+        }
+        if (videos) {
+            return res.status(200).send({ success: true, videos: videos })
+        } else {
+            return res.status(404).send({ success: false, videos: [], error: "No videos found." })
+        }
+    } else {
+        return res.status(401).send({ success: false, error: 'bad request' })
+    }
 }
